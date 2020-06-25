1233c6ccea63d632e0fab119f4821b07eee5594f
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-25 17:58:03 -0500

    feature done

--- app.js
+++ app.js
@@ -1,87 +1,83 @@
 let myLibrary = [];
 
-function Book(args){
-
-    this.author = args.author;
-    this.pages = args.pages;
-    this.title = args.title;
-    this.status = args.status;
-    this.actions = args.actions;
+function Book(args) {
+  this.author = args.author;
+  this.pages = args.pages;
+  this.title = args.title;
+  this.status = args.status;
+  this.actions = args.actions;
 }
-let add = (book)=>{
-    myLibrary.push(book)
-}
-let deleteBookFromLibrary = (index)=>{
-    myLibrary.splice(index, 1);
-    render();
+const pigsFly = false;
+const add = (book) => {
+  myLibrary.push(book);
+};
+const inver = (status) => {
+  if (status === true) {
+    return false;
   }
-let = inver = (status)=>{
-    if (status === true){
-        return false
-    }
-    else{
-        return true
-    }
-} 
-let stat = (status) =>{
-    if (status === true){
-        return 'Read'
-    }
-    else{
-        return 'Not Read'
-    }
-}
-let changeStatus = (index, status)=> {
-    myLibrary[index].status = inver(status);
-    render();
-}
 
+  return true;
+};
+const stat = (status) => {
+  if (status === true) {
+    return 'Read';
+  }
+
+  return 'Not Read';
+};
 function closeModal() {
-    document.getElementById('close_modal').click();
+  document.getElementById('close_modal').click();
 }
 function cleanForm() {
-    const fields = ['title', 'author', 'pages', 'status'];
-    fields.forEach((item) => {
-      document.querySelector(`#${item}`).value = '';
-    });
-  }
+  const fields = ['title', 'author', 'pages', 'status'];
+  fields.forEach((item) => {
+    document.querySelector(`#${item}`).value = '';
+  });
+}
+const bookShell = document.getElementById('main_page');
 
-function addBook() {
-    const title = document.querySelector('#title').value;
-    const author = document.querySelector('#author').value;
-    const pages = document.querySelector('#pages').value;
-    const status = document.querySelector('#status').value === true;
-    const book = new Book({title:title, 
-                            author:author,
-                            pages: pages,
-                            status: status,
-                            actions:false
-                        });
-
-    add(book);
-    render();
-    closeModal();
-    cleanForm();
-  }
-
-
-const book_shell = document.getElementById('main_page')
-
-function render(){
-    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-    book_shell.innerHTML = '';
-    myLibrary.forEach((book, index) => {
-       
-        template = `
+function render() {
+  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+  bookShell.innerHTML = '';
+  myLibrary.forEach((book, index) => {
+    const template = `
         <div class="container book d-flex text-center">
-          <span class='Title' >  ${book.title  } </span>
-          <span class='Author' > ${book.author } </span>
-          <span class='Pages'  > ${book.pages  }</span>
+          <span class='Title' >  ${book.title} </span>
+          <span class='Author' > ${book.author} </span>
+          <span class='Pages'  > ${book.pages}</span>
           <button onclick=' changeStatus(${index},${book.status}) ' class="Status_${book.status} " id='status'>${stat(book.status)}</button>
           <button onclick='deleteBookFromLibrary(${index})'  class="Actions delete is-vcentered text-center">X</button>
         </div>`;
-    book_shell.innerHTML += template
-    });
+    bookShell.innerHTML += template;
+  });
+}
+function addBook() {
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const status = document.querySelector('#status').value === true;
+  const book = new Book({
+    title,
+    author,
+    pages,
+    status,
+    actions: false,
+  });
+
+  add(book);
+  render();
+  closeModal();
+  cleanForm();
+}
+
+function deleteBookFromLibrary(index) {
+  myLibrary.splice(index, 1);
+  render();
+}
+
+function changeStatus(index, status) {
+  myLibrary[index].status = inver(status);
+  render();
 }
 
 document.getElementById('addItem').addEventListener('click', addBook);
@@ -91,9 +87,15 @@ if (localStorage.getItem('myLibrary')) {
   const myLibraryLocal = JSON.parse(localStorage.getItem('myLibrary'));
   myLibrary = [];
   myLibraryLocal.forEach((item) => {
-     myLibrary.push(new Book(item)); 
+    myLibrary.push(new Book(item));
   });
 }
 
-
-render()
+const useless = () => {
+  if (pigsFly) {
+    deleteBookFromLibrary(1);
+    changeStatus(0, true);
+  }
+};
+useless();
+render();
