6799b40ed4b34a7948b955e1f9cf2eff60defe74
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-25 15:49:45 -0500

    erasing entries

--- app.js
+++ app.js
@@ -13,46 +13,85 @@ let add = (book)=>{
 }
 let deleteBookFromLibrary = (index)=>{
     myLibrary.splice(index, 1);
+    console.log('INDEX DEKLETED',index)
+    render();
   }
 let updateStatusInLibrary = (index, status)=> {
 myLibrary[index].status = status;
 }
+function closeModal() {
+    document.getElementById('close_modal').click();
+}
+function cleanForm() {
+    const fields = ['title', 'author', 'pages', 'status'];
+    fields.forEach((item) => {
+      document.querySelector(`#${item}`).value = '';
+    });
+  }
 
-let uno = new Book({
-    author:'pipi',
-    pages:245,
-    title:'Pipi popo',
-    status:false,
-    actions:false
-})
-add(uno)
+function addBook() {
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const pages = document.querySelector('#pages').value;
+    const status = document.querySelector('#status').value === 'true';
+    const book = new Book({title:title, 
+                            author:author,
+                            pages: pages,
+                            status: status,
+                            actions:false
+                        });
+    add(book);
+    render();
+    closeModal();
+    cleanForm();
+  }
 
-let dos = new Book({
-    author:'pichichi',
-    pages:654,
-    title:'popotitoti',
-    status:false,
-    actions:false
-})
-add(dos)
 
-console.log(myLibrary)
-console.log('lashdlkajshdlkj')
+const book_shell = document.getElementById('main_page')
+
 function render(){
-    tableBody.innerHTML = '';
-    myLibrary.forEach((book) => {
+    localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
+    book_shell.innerHTML = '';
+    myLibrary.forEach((book, index) => {
+        let stat = (status) =>{
+            if (status == true){
+                return 'Read'
+            }
+            else{
+                return 'No read'
+            }
+        }
+
         template = `
         <div class="container book d-flex text-center">
-            <span class='Title' >Title   </span>
-            <span class='Author' >Author  </span>
-            <span class='Pages'>Pages  </span>
-            <span class='Status'>Status  </span>
-            <span class='Actions'>Actions </span>
+          <span class='Title' >  ${book.title  } </span>
+          <span class='Author' > ${book.author } </span>
+          <span class='Pages'  > ${book.pages  }</span>
+          <span class="Status " id='status'>${stat(book.status)}</span>
+          <button id = 'removeButton_${index}' class="Actions delete is-vcentered text-center">X</button>
         </div>`;
-        
-    })
+    book_shell.innerHTML += template
+    });
 }
-reder
 
+document.getElementById('addItem').addEventListener('click', addBook);
 
-    
+myLibrary.forEach((item,index) =>{
+    var ids = `removeButton_${index}`;
+    e = document.getElementById(ids);
+    d.addEventListener('click', deleteBookFromLibrary(index));
+    console.log('DIDE INE',index)
+})
+console.log(myLibrary)
+
+if (localStorage.getItem('myLibrary')) {
+  const myLibraryLocal = JSON.parse(localStorage.getItem('myLibrary'));
+  myLibrary = [];
+  myLibraryLocal.forEach((item) => {
+      
+     
+    myLibrary.push(new Book(item)); 
+  });
+}
+
+render()
