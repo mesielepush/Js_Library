5a52de60d451f8a8314390c290a98872d9fafc10
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-06-24 23:25:43 -0500

    template for books

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 let myLibrary = [];
 
-let Book = (args)=>{
+function Book(args){
+
     this.author = args.author;
     this.pages = args.pages;
     this.title = args.title;
@@ -16,3 +17,42 @@ let deleteBookFromLibrary = (index)=>{
 let updateStatusInLibrary = (index, status)=> {
 myLibrary[index].status = status;
 }
+
+let uno = new Book({
+    author:'pipi',
+    pages:245,
+    title:'Pipi popo',
+    status:false,
+    actions:false
+})
+add(uno)
+
+let dos = new Book({
+    author:'pichichi',
+    pages:654,
+    title:'popotitoti',
+    status:false,
+    actions:false
+})
+add(dos)
+
+console.log(myLibrary)
+console.log('lashdlkajshdlkj')
+function render(){
+    tableBody.innerHTML = '';
+    myLibrary.forEach((book) => {
+        template = `
+        <div class="container book d-flex text-center">
+            <span class='Title' >Title   </span>
+            <span class='Author' >Author  </span>
+            <span class='Pages'>Pages  </span>
+            <span class='Status'>Status  </span>
+            <span class='Actions'>Actions </span>
+        </div>`;
+        
+    })
+}
+reder
+
+
+    
